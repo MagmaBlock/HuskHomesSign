546843d3ba4630d7edcf016eb9631d3c557ad29d
岩浆块Magma <magmablock@qq.com>
2023-01-24 17:13:32 +0800

    create

---
+++ src/main/java/ink/magma/huskhomessign/HuskHomesSign.java
@@ -0,0 +1,145 @@
+package ink.magma.huskhomessign;
+
+import net.william278.huskhomes.api.HuskHomesAPI;
+import net.william278.huskhomes.player.OnlineUser;
+import net.william278.huskhomes.teleport.TimedTeleport;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
+import org.bukkit.block.data.type.WallSign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+public final class HuskHomesSign extends JavaPlugin implements Listener, CommandExecutor {
+    public HuskHomesAPI huskHomesAPI;
+
+    String createKey;
+    int createKeyLine;
+    String useKey;
+    int useKeyLine;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        saveDefaultConfig();
+        loadConfig();
+
+        if (Bukkit.getPluginManager().getPlugin("HuskHomes") != null) {
+            this.huskHomesAPI = HuskHomesAPI.getInstance();
+            Bukkit.getPluginManager().registerEvents(this, this);
+        } else {
+            getLogger().warning("Can't find HuskHomes install on server, this addon will not work...");
+        }
+    }
+
+    private void loadConfig() {
+        createKey = getConfig().getString("sign.create.key-word", "[warp]");
+        createKeyLine = getConfig().getInt("sign.create.key-word-line", 1) - 1;
+        useKey = ChatColor.translateAlternateColorCodes('&', getConfig().getString("sign.use.key-word", "ConfigError"));
+        useKeyLine = getConfig().getInt("sign.use.key-word-line", 2) - 1;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equals("huskhomessign") && args[0] != null && args[0].equals("reload")) {
+            saveDefaultConfig();
+            reloadConfig();
+            loadConfig();
+            sender.sendMessage("[HuskHomesSign] Reloaded.");
+            return true;
+        }
+        return false;
+    }
+
+    @EventHandler
+    public void onSignPlace(SignChangeEvent event) {
+        // if sign has a key word
+        if (String.valueOf(event.getLine(createKeyLine)).equals(createKey)) {
+            if (!event.getPlayer().hasPermission("huskhomessign.create")) return;
+            String warpName = event.getLine(createKeyLine + 1);
+            if (warpName == null || warpName.trim().isEmpty()) return;
+
+            event.setLine(0, "");
+            event.setLine(1, "");
+            event.setLine(2, "");
+            event.setLine(3, "");
+            event.setLine(useKeyLine, useKey);
+            event.setLine(useKeyLine + 1, warpName.trim());
+
+            // test this warp is valid, if not will send a message to creator
+            huskHomesAPI.getWarp(warpName.trim())
+                    .thenAccept((result) -> {
+                        if (result.isEmpty()) {
+                            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                    getConfig().getString("message.invalid-warp", "")
+                            ));
+                        }
+                    });
+
+        }
+
+        // avoid player has no permission to create "use sign",
+        // because some server player can use color codes on sign
+        if (String.valueOf(event.getLine(useKeyLine)).equals(useKey)) {
+            if (event.getPlayer().hasPermission("huskhomessign.create")) return;
+            event.setLine(useKeyLine, "");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerClickSign(PlayerInteractEvent event) {
+        // if player right-click on a sign, and check permission
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getClickedBlock() == null) return;
+        if (!(event.getClickedBlock().getBlockData() instanceof org.bukkit.block.data.type.Sign) && !(event.getClickedBlock().getBlockData() instanceof WallSign)) {
+            return;
+        }
+
+        Sign sign = (Sign) event.getClickedBlock().getState();
+        if (sign.getLine(useKeyLine).equals(useKey)) {
+
+            if (!event.getPlayer().hasPermission("huskhomessign.use")) {
+                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("message.no-use-permission", "")));
+                return;
+            }
+
+            if (sign.getLine(useKeyLine + 1).isEmpty()) return;
+            huskHomesAPI.getWarp(sign.getLine(useKeyLine + 1))
+                    .thenAccept((result) -> {
+                        if (result.isPresent()) {
+                            OnlineUser user = huskHomesAPI.adaptUser(event.getPlayer());
+                            huskHomesAPI.teleportBuilder(user)
+                                    .setTarget(result.get())
+                                    .toTimedTeleport()
+                                    .thenAccept(TimedTeleport::execute);
+                        } else {
+                            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                    getConfig().getString("message.invalid-warp", "")
+                            ));
+                        }
+                    });
+
+        }
+
+
+    }
+
+
+}
