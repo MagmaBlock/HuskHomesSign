a3dc6202ffa6668928eb996202cab45893778b6c
岩浆块Magma <magmablock@qq.com>
2024-09-21 21:42:36 +0800

    Reload command now in a single class, with autocomplete support

---
+++ src/main/java/ink/magma/huskhomessign/commands/ReloadCommand.java
@@ -0,0 +1,37 @@
+package ink.magma.huskhomessign.commands;
+
+import ink.magma.huskhomessign.HuskHomesSign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ReloadCommand implements TabExecutor {
+    private final HuskHomesSign instance;
+
+    public ReloadCommand(HuskHomesSign instance) {
+        this.instance = instance;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (Objects.requireNonNullElse(args[0], "").equals("reload")) {
+            instance.saveDefaultConfig();
+            instance.reloadConfig();
+            instance.readConfig();
+            sender.sendMessage("[HuskHomesSign] Reloaded.");
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length <= 1) {
+            return List.of("reload");
+        }
+        return List.of();
+    }
+}
